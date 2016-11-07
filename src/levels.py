8cfe71afb60d69f3a74ebf55ed388829660e6158
TitovS <m.titov.sergey@gmail.com>
2016-11-07 22:07:27 +0300

    new patterns

--- src/levels.py
+++ src/levels.py
@@ -1,28 +1,39 @@
-level_patterns = [["P                  P",
+from random import choice
+
+level_patterns = [["P             UUU  P",
                    "P                  P",
+                   "P        AAA       P",
                    "P                  P",
-                   "P                  P",
-                   "P   PPP            P"],
+                   "P   UUU            P"],
                   ["P                  P",
-                   "P                  P",
-                   "P                  P",
-                   "PPPPPPPP           P",
+                   "P   A          A   P",
+                   "P        UU        P",
+                   "P   A          A   P",
+                   "P                  P"]
+                  ["P                  P",
+                   "P   U          U   P",
+                   "P        AA        P",
+                   "P   U          U   P",
+                   "P                  P"]
+                  ["P                  P",
+                   "PA  U   A   U   A  P",
+                   "P A  U   A   U   A P",
+                   "P  A  U   A   U   AP",
                    "P                  P"]]
 
 
-def Level_generate(width, height):
-    from random import choice
+def level_generate(width, height):
 
-    set = ['A', 'P', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    set_of_elements = ['A', 'P', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     level = []
     skip = 3
 
-    for k in range(height / 5 + skip):
+    for k in range(height // (5 + skip)):
 
         level.append(choice(level_patterns))
 
-        for i in skip:
-            string_val = "P" + "".join(choice(set) for i in range(width - 2)) + "P"
+        for i in range(skip):
+            string_val = "P" + "".join(choice(set_of_elements) for i in range(width - 2)) + "P"
             level.append(string_val)
 
     return level
